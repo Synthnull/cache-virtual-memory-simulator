12d5a445f229a5200f72e8dedb5cfe6e18522423
Erik <ej.erik.jimenez@gmail.com>
2026-03-28 20:38:48 -0500

    comment

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
-#include <stdbool.h>
 #include "cmd_parser.h"
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
 	Parameters *parameters = initParameters(3);
-   bool err = parseCommandLine(argc, argv, parameters);
-   
-	if (err) {
-		
-	}
+	bool err = parseCommandLine(argc, argv, parameters);
 
-   freeParameters(parameters);
+	if (err) {
+		freeParameters(parameters);
+		return 1;
+	}
+   
+   //call functions
+
+	freeParameters(parameters);
+
 	return 0;
 }
