6b670fd031cf3252055e2754517547bd1e465bf1
chris341234 <coletochristopher@gmail.com>
2026-03-30 00:10:38 -0500

    Update memory_calculations.h

--- memory_calculations.h
+++ memory_calculations.h
@@ -1,23 +1,20 @@
-
 #ifndef MEMORY_CALCULATIONS_H
 #define MEMORY_CALCULATIONS_H
 
-#include <stdint.h>
-
 typedef struct {
-    uint32_t physical_memory_mb;
+    int physical_memory_mb;
     double os_percent;
-    uint32_t num_trace_files;
+    int num_trace_files;
 
-    uint64_t number_physical_pages;
-    uint64_t number_pages_for_system;
-    uint32_t page_table_entry_size_bits;
-    uint64_t total_ram_for_page_tables_bytes;
+    unsigned long long number_physical_pages;
+    unsigned long long number_pages_for_system;
+    unsigned int page_table_entry_size_bits;
+    unsigned long long total_ram_for_page_tables_bytes;
 } MemoryCalculationResults;
 
-int calculate(uint32_t physical_memory_mb,
+int calculate(int physical_memory_mb,
               double os_percent,
-              uint32_t num_trace_files,
+              int num_trace_files,
               MemoryCalculationResults *results);
 
 #endif
