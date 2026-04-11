03c1be30e6096769603209c5ff4fcb1bb5f02ce4
chris341234 <coletochristopher@gmail.com>
2026-04-11 16:21:16 -0500

    Update trace_parser.c

--- trace_parser.c
+++ trace_parser.c
@@ -1,45 +1,43 @@
+#include "trace_parser.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
-#include "trace_parser.h"
 
 #define LINE_BUF 256
-#define EIP_ADDR_OFFSET  10
-#define DST_ADDR_OFFSET  6
-#define DST_DATA_OFFSET  15
-#define SRC_ADDR_OFFSET  27
-#define SRC_DATA_OFFSET  36
-
+#define EIP_ADDR_OFFSET 10
+#define DST_ADDR_OFFSET 6
+#define DST_DATA_OFFSET 15
+#define SRC_ADDR_OFFSET 27
+#define SRC_DATA_OFFSET 36
 
 static TraceEntry pending[2];
 static int pendingCount = 0;
 static int pendingIndex = 0;
 
-
 static int isInvalid(const char *dataField) {
     return strncmp(dataField, "--------", 8) == 0;
 }
-
 
 static int readLine(FILE *f, char *buf, int size) {
     while (fgets(buf, size, f) != NULL) {
         int len = (int)strlen(buf);
 
-        
-        while (len > 0 && (buf[len-1] == '\n' || buf[len-1] == '\r')) {
+        while (len > 0 && (buf[len - 1] == '\n' || buf[len - 1] == '\r')) {
             buf[--len] = '\0';
         }
 
-        if (len > 0) return 1; 
+        if (len > 0) {
+            return 1;
+        }
     }
-    return 0; 
+
+    return 0;
 }
 
 int getNextTraceEntry(FILE *file, TraceEntry *entry) {
     char eipLine[LINE_BUF];
     char dstLine[LINE_BUF];
 
-    
     if (pendingIndex < pendingCount) {
         *entry = pending[pendingIndex++];
         return 1;
@@ -48,44 +46,57 @@ int getNextTraceEntry(FILE *file, TraceEntry *entry) {
     pendingCount = 0;
     pendingIndex = 0;
 
-    
     while (1) {
-        if (!readLine(file, eipLine, LINE_BUF)) return 0;
-        if (strncmp(eipLine, "EIP", 3) == 0) break;
+        if (!readLine(file, eipLine, LINE_BUF)) {
+            return 0;
+        }
+
+        if (strncmp(eipLine, "EIP", 3) == 0) {
+            break;
+        }
     }
 
-    
-    unsigned int instrAddr = 0;
-    if (sscanf(eipLine + EIP_ADDR_OFFSET, "%x", &instrAddr) != 1) {
-        return 0; 
+    {
+        unsigned int instrAddr = 0;
+
+        if (sscanf(eipLine + EIP_ADDR_OFFSET, "%x", &instrAddr) != 1) {
+            return 0;
+        }
+
+        entry->operation = 'R';
+        entry->virAddr = (uint32_t)instrAddr;
+        entry->instructionComplete = 1; /* changed below if dst/src exist */
     }
 
-    entry->operation = 'R';
-    entry->virAddr   = (uint32_t)instrAddr;
-
-    
     if (!readLine(file, dstLine, LINE_BUF)) {
         return 1;
     }
 
     if (strncmp(dstLine, "dstM", 4) == 0) {
-
-        
         unsigned int dstAddr = 0;
+        unsigned int srcAddr = 0;
+
         if (sscanf(dstLine + DST_ADDR_OFFSET, "%x", &dstAddr) == 1) {
             if (dstAddr != 0 && !isInvalid(dstLine + DST_DATA_OFFSET)) {
                 pending[pendingCount].operation = 'W';
-                pending[pendingCount].virAddr   = (uint32_t)dstAddr;
+                pending[pendingCount].virAddr = (uint32_t)dstAddr;
+                pending[pendingCount].instructionComplete = 0;
                 pendingCount++;
             }
         }
-        unsigned int srcAddr = 0;
+
         if (sscanf(dstLine + SRC_ADDR_OFFSET, "%x", &srcAddr) == 1) {
             if (srcAddr != 0 && !isInvalid(dstLine + SRC_DATA_OFFSET)) {
                 pending[pendingCount].operation = 'R';
-                pending[pendingCount].virAddr   = (uint32_t)srcAddr;
+                pending[pendingCount].virAddr = (uint32_t)srcAddr;
+                pending[pendingCount].instructionComplete = 0;
                 pendingCount++;
             }
+        }
+
+        if (pendingCount > 0) {
+            entry->instructionComplete = 0;
+            pending[pendingCount - 1].instructionComplete = 1;
         }
     }
 
