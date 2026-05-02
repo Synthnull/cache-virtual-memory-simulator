052220b313e706e62d913be081b8e580df5cfa94
isaxcm <isaacmavoungou@gmail.com>
2026-05-02 13:03:56 -0500

    Refactor miss handling in processMiss function

--- miss_counter.c
+++ miss_counter.c
@@ -1,80 +1,33 @@
 #include "miss_counter.h"
 
+void processMiss(MissType missType, char instType, int accessBytes,
+                 CacheSimulationResults *results) {
 
-
-static int getEvictCol(Cache *cachePtr, int index) {
-    
-    static int *rrCounters = NULL;
-    static int  rrRows     = 0;
-
-    
-    if (rrCounters == NULL || rrRows != cachePtr->rows) {
-        if (rrCounters != NULL) free(rrCounters);
-        rrCounters = calloc(cachePtr->rows, sizeof(int));
-        rrRows     = cachePtr->rows;
-    }
-
-    int col = rrCounters[index];
-    rrCounters[index] = (rrCounters[index] + 1) % cachePtr->associativity;
-    return col;
-}
-
-
-static void loadBlock(Cache *cachePtr, int tag, int index, char operation) {
-    int i;
+    if (results == NULL)
+        return;
 
     
-    for (i = 0; i < cachePtr->associativity; i++) {
-        if (cachePtr->cacheBlocks[index][i].validbit == 0) {
-            cachePtr->cacheBlocks[index][i].tag      = tag;
-            cachePtr->cacheBlocks[index][i].validbit = 1;
-            cachePtr->cacheBlocks[index][i].dirtybit = (operation == 'W') ? 1 : 0;
-            return;
-        }
+    results->totalAccesses++;
+
+    
+    if (instType == 'R') {
+        results->instructionBytes += accessBytes;
+    } else if (instType == 'W') {
+        results->destBytes += accessBytes;
     }
 
     
-    int col = getEvictCol(cachePtr, index);
-    cachePtr->cacheBlocks[index][col].tag      = tag;
-    cachePtr->cacheBlocks[index][col].validbit = 1;
-    cachePtr->cacheBlocks[index][col].dirtybit = (operation == 'W') ? 1 : 0;
-}
-
-void processAccess(Cache *cachePtr, int phyAddr, char operation,
-                   CacheSimulationResults *results) {
-    int tag, index, offset;
-
-    parseAddress(phyAddr, &tag, &index, &offset,
-                 cachePtr->tagSize, cachePtr->indexSize);
-
-    
-    int i;
-    for (i = 0; i < cachePtr->associativity; i++) {
-        if (cachePtr->cacheBlocks[index][i].validbit == 1 &&
-            cachePtr->cacheBlocks[index][i].tag == tag) {
-
-            
-            if (operation == 'W')
-                cachePtr->cacheBlocks[index][i].dirtybit = 1;
-            return; 
-        }
-    }
-
-    
-    int foundEmpty = 0;
-    for (i = 0; i < cachePtr->associativity; i++) {
-        if (cachePtr->cacheBlocks[index][i].validbit == 0) {
-            foundEmpty = 1;
+    switch (missType) {
+        case COMPULSORY:
+            results->compulsoryMisses++;
             break;
-        }
+        case CONFLICT:
+            results->conflictMisses++;
+            break;
+        case CAPACITY:
+            results->capacityMisses++;
+            break;
+        case NO_MISS:
+            break;
     }
-
-    if (foundEmpty) {
-        results->compulsoryMisses++;  
-    } else {
-        results->conflictMisses++;   
-    }
-
-    
-    loadBlock(cachePtr, tag, index, operation);
 }
