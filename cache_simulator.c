a8e119090e6f5ff0c20338754731ee3e565f1136
isaxcm <isaacmavoungou@gmail.com>
2026-05-02 14:45:56 -0500

    Refactor runCacheSimulation for better error handling

--- cache_simulator.c
+++ cache_simulator.c
@@ -1,42 +1,68 @@
 #include "cache_simulator.h"
 #include "address_parser.h"
 #include "cpu_cache.h"
+#include "cache_replace.h"
 #include "error.h"
 
 MissType runCacheSimulation(Cache *cachePtr, CacheOutput *cacheParameters,
-									 CacheSimulationResults *results, int phyAddr,
-									 char instType) {
-	MissType missType;
-	int cacheCol = 0;
-	int tag;
-	int index;
-	int offset;
+                             CacheSimulationResults *results, int phyAddr,
+                             char instType) {
+    MissType missType;
+    int cacheCol = 0;
+    int tag;
+    int index;
+    int offset;
 
-	parseAddress(phyAddr, &tag, &index, &offset, cacheParameters->tag_size,
-					 cacheParameters->index_size);
-	missType = readCache(cachePtr, phyAddr, &cacheCol);
-	if (instType == 'R') {
-		switch (missType) {
-		case CONFLICT:
-			break;
-		case COMPULSORY:
-			break;
-		case CAPACITY:
-			break;
-		case NO_MISS:
-			break;
-		}
-	} else {
-		switch (missType) {
-		case CONFLICT:
-			break;
-		case COMPULSORY:
-			break;
-		case CAPACITY:
-			break;
-		case NO_MISS:
-			break;
-		}
-	}
-	return 0;
+    if (cachePtr == NULL || cacheParameters == NULL || results == NULL)
+        return NO_MISS;
+
+    parseAddress(phyAddr, &tag, &index, &offset, cacheParameters->tag_size,
+                 cacheParameters->index_size);
+
+    missType = readCache(cachePtr, phyAddr, &cacheCol);
+
+    results->totalAccesses++;
+    if (instType == 'R') {
+        results->instructionBytes += cacheParameters->block_size;
+    } else {
+        results->destBytes += cacheParameters->block_size;
+    }
+
+    if (instType == 'R') {
+        switch (missType) {
+        case CONFLICT:
+            results->conflictMisses++;
+            roundRobinReplace(cachePtr, index, tag, offset);
+            break;
+        case COMPULSORY:
+            results->compulsoryMisses++;
+            roundRobinReplace(cachePtr, index, tag, offset);
+            break;
+        case CAPACITY:
+            results->capacityMisses++;
+            roundRobinReplace(cachePtr, index, tag, offset);
+            break;
+        case NO_MISS:
+            break;
+        }
+    } else {
+        switch (missType) {
+        case CONFLICT:
+            results->conflictMisses++;
+            roundRobinReplace(cachePtr, index, tag, offset);
+            break;
+        case COMPULSORY:
+            results->compulsoryMisses++;
+            roundRobinReplace(cachePtr, index, tag, offset);
+            break;
+        case CAPACITY:
+            results->capacityMisses++;
+            roundRobinReplace(cachePtr, index, tag, offset);
+            break;
+        case NO_MISS:
+            break;
+        }
+    }
+
+    return missType;
 }
