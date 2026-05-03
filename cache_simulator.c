1cf1ce1d8fbd95569b3e15cbf400412f9ead3508
chris341234 <coletochristopher@gmail.com>
2026-05-03 02:59:05 -0500

    Update cache_simulator.c

--- cache_simulator.c
+++ cache_simulator.c
@@ -2,65 +2,65 @@
 #include "address_parser.h"
 #include "cpu_cache.h"
 #include "cache_replace.h"
+#include "cmd_parser.h"
 #include "error.h"
 
+static void replaceCacheBlock(Cache *cachePtr, ReplacementPolicy policy,
+										int index, int tag, int offset) {
+	if (policy == RND) {
+		randomReplace(cachePtr, index, tag, offset);
+	} else {
+		roundRobinReplace(cachePtr, index, tag, offset);
+	}
+}
+
 MissType runCacheSimulation(Cache *cachePtr, CacheOutput *cacheParameters,
-                             CacheSimulationResults *results, int phyAddr, char instType) {
-    MissType missType;
-    int cacheCol = 0;
-    int tag;
-    int index;
-    int offset;
+									  CacheSimulationResults *results, int phyAddr,
+									  char instType, ReplacementPolicy policy) {
+	MissType missType;
+	int cacheCol = 0;
+	int tag;
+	int index;
+	int offset;
 
-    if (cachePtr == NULL || cacheParameters == NULL || results == NULL)
-        return NO_MISS;
+	if (cachePtr == NULL || cacheParameters == NULL || results == NULL) {
+		return NO_MISS;
+	}
 
-    parseAddress(phyAddr, &tag, &index, &offset, cacheParameters->tag_size,
-                 cacheParameters->index_size);
+	parseAddress(phyAddr, &tag, &index, &offset, cacheParameters->tag_size,
+					 cacheParameters->index_size);
 
-    missType = readCache(cachePtr, phyAddr, &cacheCol);
+	missType = readCache(cachePtr, phyAddr, &cacheCol);
 
-    if (instType == 'R') {
-        results->totalAccesses++;
-        results->instructionBytes += cacheParameters->block_size;
-        switch (missType) {
-        case CONFLICT:
-            results->conflictMisses++;
-            roundRobinReplace(cachePtr, index, tag, offset);
-            break;
-        case COMPULSORY:
-            results->compulsoryMisses++;
-            cachePtr->cacheBlocks[index][cacheCol].tag     = tag;
-            cachePtr->cacheBlocks[index][cacheCol].validbit = 1;
-            break;
-        case CAPACITY:
-            results->capacityMisses++;
-            roundRobinReplace(cachePtr, index, tag, offset);
-            break;
-        case NO_MISS:
-            break;
-        }
-    } else {
-        results->totalAccesses++;
-        results->destBytes += cacheParameters->block_size;
-        switch (missType) {
-        case CONFLICT:
-            results->conflictMisses++;
-            roundRobinReplace(cachePtr, index, tag, offset);
-            break;
-        case COMPULSORY:
-            results->compulsoryMisses++;
-            cachePtr->cacheBlocks[index][cacheCol].tag      = tag;
-            cachePtr->cacheBlocks[index][cacheCol].validbit = 1;
-            break;
-        case CAPACITY:
-            results->capacityMisses++;
-            roundRobinReplace(cachePtr, index, tag, offset);
-            break;
-        case NO_MISS:
-            break;
-        }
-    }
+	results->totalAccesses++;
 
-    return missType;
+	if (instType == 'R') {
+		results->instructionBytes += cacheParameters->block_size;
+	} else {
+		results->destBytes += cacheParameters->block_size;
+	}
+
+	switch (missType) {
+	case CONFLICT:
+		results->conflictMisses++;
+		replaceCacheBlock(cachePtr, policy, index, tag, offset);
+		break;
+
+	case COMPULSORY:
+		results->compulsoryMisses++;
+		cachePtr->cacheBlocks[index][cacheCol].tag = tag;
+		cachePtr->cacheBlocks[index][cacheCol].validbit = 1;
+		cachePtr->cacheBlocks[index][cacheCol].dirtybit = 0;
+		break;
+
+	case CAPACITY:
+		results->capacityMisses++;
+		replaceCacheBlock(cachePtr, policy, index, tag, offset);
+		break;
+
+	case NO_MISS:
+		break;
+	}
+
+	return missType;
 }
