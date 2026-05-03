92d31fe99c87437768a5cc733bb9200af7ffcc66
chris341234 <coletochristopher@gmail.com>
2026-05-03 02:56:52 -0500

    Update cpu_cache.c

--- cpu_cache.c
+++ cpu_cache.c
@@ -6,13 +6,12 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-Cache *initCache(int associativity,
-					  CacheOutput cacheCalcResults) {
+Cache *initCache(int associativity, CacheOutput cacheCalcResults) {
 	int i;
 	Cache *newCache = malloc(sizeof(Cache));
 
 	if (newCache == NULL) {
-		return NULL; // alloc err
+		return NULL;
 	}
 
 	newCache->rows = cacheCalcResults.total_rows;
@@ -20,16 +19,38 @@ Cache *initCache(int associativity,
 	newCache->indexSize = cacheCalcResults.index_size;
 	newCache->tagSize = cacheCalcResults.tag_size;
 
+	newCache->nextReplace = calloc(newCache->rows, sizeof(int));
+
+	if (newCache->nextReplace == NULL) {
+		free(newCache);
+		return NULL;
+	}
+
 	newCache->cacheBlocks =
 		 (CacheBlock **)calloc(newCache->rows, sizeof(CacheBlock *));
+
+	if (newCache->cacheBlocks == NULL) {
+		free(newCache->nextReplace);
+		free(newCache);
+		return NULL;
+	}
 
 	for (i = 0; i < newCache->rows; i++) {
 		newCache->cacheBlocks[i] =
 			 (CacheBlock *)calloc(newCache->associativity, sizeof(CacheBlock));
+
 		if (newCache->cacheBlocks[i] == NULL) {
+			while (--i >= 0) {
+				free(newCache->cacheBlocks[i]);
+			}
+
+			free(newCache->cacheBlocks);
+			free(newCache->nextReplace);
+			free(newCache);
 			return NULL;
 		}
 	}
+
 	return newCache;
 }
 
@@ -47,32 +68,42 @@ MissType readCache(Cache *cachePtr, int phyAddr, int *cacheCol) {
 		if (tag == cachePtr->cacheBlocks[index][i].tag &&
 			 cachePtr->cacheBlocks[index][i].validbit == 1) {
 			*cacheCol = i;
-			return NO_MISS; // hit
+			return NO_MISS;
 		} else if (cachePtr->cacheBlocks[index][i].validbit == 0) {
-			foundEmpty = true; // no data
+			*cacheCol = i;
+			foundEmpty = true;
 		}
 	}
-	return foundEmpty ? COMPULSORY : CONFLICT; // index full
+
+	return foundEmpty ? COMPULSORY : CONFLICT;
 }
 
 int flushCache(Cache *cachePtr, PageTable *processPtr) {
 	int i;
-   int currCacheCol;
-   int index;
+	int currCacheCol;
+	int index;
+
+	if (cachePtr == NULL || processPtr == NULL) {
+		return 1;
+	}
 
 	for (i = 0; i < processPtr->numPages; i++) {
-      int currPhyAddr = processPtr->pages[i].phyAddr;
-      parseAddress(currPhyAddr, NULL, &index, NULL, cachePtr->tagSize, cachePtr->indexSize);
+		int currPhyAddr = processPtr->pages[i].phyAddr;
 
-      if(readCache(cachePtr, currPhyAddr, &currCacheCol) == NO_MISS) {
-         cachePtr->cacheBlocks[index][currCacheCol].validbit = 0;
-      }
-   }
+		parseAddress(currPhyAddr, NULL, &index, NULL, cachePtr->tagSize,
+						 cachePtr->indexSize);
+
+		if (readCache(cachePtr, currPhyAddr, &currCacheCol) == NO_MISS) {
+			cachePtr->cacheBlocks[index][currCacheCol].validbit = 0;
+		}
+	}
+
 	return 0;
 }
 
 int freeCache(Cache *cachePtr) {
 	int i;
+
 	if (cachePtr == NULL) {
 		return 1;
 	}
@@ -84,6 +115,8 @@ int freeCache(Cache *cachePtr) {
 	}
 
 	free(cachePtr->cacheBlocks);
+	free(cachePtr->nextReplace);
 	free(cachePtr);
+
 	return 0;
 }
